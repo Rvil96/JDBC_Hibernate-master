5842fbbcdb6b10d8a81847030edb904a1898d4dd
Ravil <r.ishkulov96@gmail.com>
2024-03-25 12:49:32 +0400

    Fix mistake4

--- JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
+++ JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,22 +3,32 @@ package jm.task.core.jdbc.util;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private final static String URL = "jdbc:mysql://localhost:3306/testdb";
     private final static String NAME = "root";
     private final static String PASSWORD = "root";
+    private final static String DRIVER = "com.mysql.jdbc.Driver";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, NAME, PASSWORD);
     }
 
     public static SessionFactory getSessionFactory() {
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, DRIVER);
+        properties.put(Environment.URL, URL);
+        properties.put(Environment.USER, NAME);
+        properties.put(Environment.PASS, PASSWORD);
+
         return new Configuration()
+                .setProperties(properties)
                 .addAnnotatedClass(User.class)
                 .buildSessionFactory();
     }
